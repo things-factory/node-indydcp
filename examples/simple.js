d036aa60c7bd4efd555e45c3500892877b423530
Hearty, Oh <heartyoh@hatiolab.com>
2020-05-17 17:32:23 +0900

    remove build to npm ignore

--- examples/simple.js
+++ examples/simple.js
@@ -1,4 +1,4 @@
-import { IndyDCPClient, sleep } from '@things-factory/node-indydcp'
+const { IndyDCPClient, sleep } = require('@things-factory/node-indydcp')
 
 async function waitForState(client, checkFn) {
   var robotStatus = await client.getRobotStatus()
@@ -8,17 +8,19 @@ async function waitForState(client, checkFn) {
   }
 }
 
-var client = new IndyDCPClient('192.168.1.207', 'NRMK-Indy7')
-await client.connect()
+;(async function () {
+  var client = new IndyDCPClient('192.168.1.207', 'NRMK-Indy7')
+  await client.connect()
 
-await client.go_home()
-console.log(await client.getRobotStatus())
+  await client.go_home()
+  console.log(await client.getRobotStatus())
 
-await waitForState(client, status => !status.is_busy)
+  await waitForState(client, status => !status.is_busy)
 
-await client.go_zero()
-console.log(await client.getRobotStatus())
+  await client.go_zero()
+  console.log(await client.getRobotStatus())
 
-await waitForState(client, status => !status.is_busy)
+  await waitForState(client, status => !status.is_busy)
 
-client.disconnect()
+  client.disconnect()
+})()
